6eba651aa08002d1f4b8b8daae649eedb56d9035
Gabriela360 <g.r.dimitrova2021@pmg-pernik.com>
2026-01-14 09:37:55 +0200

    chast4

--- WebShopApp/WebShopApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
+++ WebShopApp/WebShopApp.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -8,7 +8,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using WebShopApp.Infrastructure.Data;
 using WebShopApp.Infrastructure.Data.Domain;
 
 namespace WebShopApp.Infrastructure.Data.Infrastructure
@@ -18,70 +17,33 @@ namespace WebShopApp.Infrastructure.Data.Infrastructure
         public static async Task<IApplicationBuilder> PrepareDatabase(this IApplicationBuilder app)
         {
             using var serviceScope = app.ApplicationServices.CreateScope();
+
             var services = serviceScope.ServiceProvider;
+
             await RoleSeeder(services);
             await SeedAdministrator(services);
-            var dataCatecory = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            SeedCategories(dataCatecory);
+
+            var dataCategory = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            SeedCategories(dataCategory);
+
             var dataBrand = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            SeedBrands(dataCatecory);
+            SeedBrands(dataBrand);
+
             return app;
-
         }
-
-        private static void SeedCategories(ApplicationDbContext dataCategory)
-        {
-            if (dataCategory.Categories.Any())
-            {
-                return;
-            }
-
-            dataCategory.Categories.AddRange(new[]
-            {
-                new Category {CategoryName="Laptop"},
-                new Category {CategoryName="Computer"},
-                new Category {CategoryName="Monitor"},
-                new Category {CategoryName="Accessory"},
-                new Category {CategoryName="TV"},
-                new Category {CategoryName="Mobile phone"},
-                new Category {CategoryName="Smart watch"}
-            });
-
-            dataCategory.SaveChanges();
-
-        }
-
-        private static void SeedBrands(ApplicationDbContext dataBrand)
-        {
-            if (dataBrand.Brands.Any())
-            {
-                return;
-            }
-
-            dataBrand.Brands.AddRange(new[]
-            {
-                new Brand {BrandName="Acer"},
-                new Brand {BrandName="Asus"},
-                new Brand {BrandName="Apple"},
-                new Brand {BrandName="Dell"},
-                new Brand {BrandName="HP"},
-                new Brand {BrandName="Huawei"},
-                new Brand {BrandName="Lenovo"},
-                new Brand {BrandName="Samsung"},
-            });
-
-            dataBrand.SaveChanges();
-        }
-
 
         private static async Task RoleSeeder(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
             string[] roleNames = { "Administrator", "Client" };
+
             IdentityResult roleResult;
+
             foreach (var role in roleNames)
             {
                 var roleExist = await roleManager.RoleExistsAsync(role);
+
                 if (!roleExist)
                 {
                     roleResult = await roleManager.CreateAsync(new IdentityRole(role));
@@ -92,6 +54,7 @@ namespace WebShopApp.Infrastructure.Data.Infrastructure
         private static async Task SeedAdministrator(IServiceProvider serviceProvider)
         {
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
             if (await userManager.FindByNameAsync("admin") == null)
             {
                 ApplicationUser user = new ApplicationUser();
@@ -102,13 +65,55 @@ namespace WebShopApp.Infrastructure.Data.Infrastructure
                 user.Address = "admin address";
                 user.PhoneNumber = "0888888888";
 
-                var result = await userManager.CreateAsync(user, "Admin123456");
+                var result = await userManager.CreateAsync
+                (user, "Admin123456");
+
                 if (result.Succeeded)
                 {
-                    userManager.AddToRoleAsync(user, "Adminitrator").Wait();
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
                 }
-
             }
+        }
+
+        private static void SeedCategories(ApplicationDbContext dataCategory)
+        {
+            if (dataCategory.Categories.Any())
+            {
+                return;
+            }
+            dataCategory.Categories.AddRange(new[]
+            {
+                new Category {CategoryName="Laptop"},
+                new Category {CategoryName="Computer"},
+                new Category {CategoryName="Monitor"},
+                new Category {CategoryName="Accessory"},
+                 new Category {CategoryName="TV"},
+                new Category {CategoryName="Mobile phone"},
+                new Category {CategoryName="Smart watch"}
+
+            });
+            dataCategory.SaveChanges();
+        }
+
+        private static void SeedBrands(ApplicationDbContext dataBrand)
+        {
+            if (dataBrand.Brands.Any())
+            {
+                return;
+            }
+            dataBrand.Brands.AddRange(new[]
+            {
+                new Brand {BrandName="Acer"},
+                new Brand {BrandName="Asus"},
+                new Brand {BrandName="Apple"},
+                new Brand {BrandName="Dell"},
+                new Brand {BrandName="HP"},
+                new Brand {BrandName="Huawei"},
+                new Brand {BrandName="Lenovo"},
+                new Brand {BrandName="Samsung"},
+
+            });
+            dataBrand.SaveChanges();
         }
     }
 }
--- WebShopApp/WebShopApp/Controllers/ProductController.cs
+++ WebShopApp/WebShopApp/Controllers/ProductController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 using WebShopApp.Core.Contacts;
+using WebShopApp.Infrastructure.Data.Domain;
 using WebShopApp.Models.Brand;
 using WebShopApp.Models.Category;
 using WebShopApp.Models.Product;
 
 namespace WebShopApp.Controllers
 {
+
     public class ProductController : Controller
     {
         private readonly IProductService _productService;
@@ -47,46 +50,57 @@ namespace WebShopApp.Controllers
         // GET: ProductController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Product item = _productService.GetProductById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            ProductDetailsVM product = new ProductDetailsVM()
+            {
+                Id = item.Id,
+                ProductName = item.ProductName,
+                BrandId = item.BrandId,
+                BrandName = item.Brand.BrandName,
+                CategoryId = item.CategoryId,
+                CategoryName = item.Category.CategoryName,
+                Picture = item.Picture,
+                Quantity = item.Quantity,
+                Price = item.Price,
+                Discount = item.Discount
+            };
+            return View(product);
         }
 
         // GET: ProductController/Create
         public ActionResult Create()
         {
-            var product = new ProductIndexVM();
+            var product = new ProductCreateVM();
             product.Brands = _brandService.GetBrands()
                 .Select(x => new BrandPairVM()
                 {
                     Id = x.Id,
                     Name = x.BrandName
                 }).ToList();
-
             product.Categories = _categoryService.GetCategories()
-                .Select(x => new CategoryPairVM()
-                {
-                    Id = x.Id,
-                    Name = x.CategoryName
-                }).ToList();
-
+               .Select(x => new CategoryPairVM()
+               {
+                   Id = x.Id,
+                   Name = x.CategoryName
+               }).ToList();
             return View(product);
         }
 
         // POST: ProductController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([FromForm] ProductIndexVM product)
+        public ActionResult Create([FromForm] ProductCreateVM product)
         {
             if (ModelState.IsValid)
             {
-                var createdId = _productService.Create(
-                    product.ProductName,
-                    product.BrandId,
-                    product.CategoryId,
-                    product.Picture,
-                    product.Quantity,
-                    product.Price,
-                    product.Discount);
-
+                var createdId = _productService.Create(product.ProductName, product.BrandId,
+                product.CategoryId, product.Picture,
+                product.Quantity, product.Price, product.Discount);
                 if (createdId)
                 {
                     return RedirectToAction(nameof(Index));
@@ -96,31 +110,92 @@ namespace WebShopApp.Controllers
             return View();
         }
 
+
         // GET: ProductController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Product product = _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ProductEditVM updatedProduct = new ProductEditVM()
+            {
+                Id = product.Id,
+                ProductName = product.ProductName,
+                BrandId = product.BrandId,
+                //BrandName = product.Brand.BrandName,
+                CategoryId = product.CategoryId,
+                // CategoryName = product.Category.CategoryName,
+                Picture = product.Picture,
+                Quantity = product.Quantity,
+                Price = product.Price,
+                Discount = product.Discount
+            };
+            updatedProduct.Brands = _brandService.GetBrands()
+    .Select(b => new BrandPairVM()
+    {
+        Id = b.Id,
+        Name = b.BrandName
+    })
+    .ToList();
+
+            updatedProduct.Categories = _categoryService.GetCategories()
+                .Select(c => new CategoryPairVM()
+                {
+                    Id = c.Id,
+                    Name = c.CategoryName
+                })
+                .ToList();
+
+            return View(updatedProduct);
         }
+
 
         // POST: ProductController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, ProductEditVM product)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var updated = _productService.Update(id, product.ProductName, product.BrandId,
+                    product.CategoryId, product.Picture,
+                    product.Quantity, product.Price, product.Discount);
+
+                if (updated)
+                {
+                    return this.RedirectToAction("Index");
+                }
             }
-            catch
-            {
-                return View();
-            }
+
+            return View(product);
         }
 
         // GET: ProductController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Product item = _productService.GetProductById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            ProductDeleteVM product = new ProductDeleteVM()
+            {
+                Id = item.Id,
+                ProductName = item.ProductName,
+                BrandId = item.BrandId,
+                BrandName = item.Brand.BrandName,
+                CategoryId = item.CategoryId,
+                CategoryName = item.Category.CategoryName,
+                Picture = item.Picture,
+                Quantity = item.Quantity,
+                Price = item.Price,
+                Discount = item.Discount
+            };
+
+            return View(product);
         }
 
         // POST: ProductController/Delete/5
@@ -128,14 +203,21 @@ namespace WebShopApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
         {
-            try
+            var deleted = _productService.RemoveById(id);
+
+            if (deleted)
             {
-                return RedirectToAction(nameof(Index));
+                return this.RedirectToAction("Success");
             }
-            catch
+            else
             {
                 return View();
             }
+        }
+
+        public IActionResult Success()
+        {
+            return View();
         }
     }
 }
